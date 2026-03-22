e5316e4ece76e90842f44c4e3a8931a9473f97d5
Peter Esztari <esztari.peter@gmail.com>
2026-03-22 14:09:40 +0100

    feat: workflow status, context links, wider drawer in detail

--- ui/detail.go
+++ ui/detail.go
@@ -30,7 +30,12 @@ func (s *server) handleDetail(w http.ResponseWriter, r *http.Request) {
 
 	data := s.buildDetailData(ctx, item, actor)
 	sse := datastar.NewSSE(w, r)
-	_ = ds.Send.Drawer(sse, detailDrawer(data))
+
+	var drawerOpts []ds.DrawerOption
+	if data.DrawerSize == DrawerSizeWide {
+		drawerOpts = append(drawerOpts, ds.WithDrawerMaxWidth("max-w-2xl"))
+	}
+	_ = ds.Send.Drawer(sse, detailDrawer(data), drawerOpts...)
 }
 
 // buildDetailData constructs the detailData for rendering the detail drawer.
@@ -38,23 +43,49 @@ func (s *server) handleDetail(w http.ResponseWriter, r *http.Request) {
 // rendering to the provider.
 func (s *server) buildDetailData(ctx context.Context, item inbox.Item, actor string) detailData {
 	data := detailData{
-		Item:     item,
-		Actor:    actor,
-		BasePath: s.cfg.basePath,
+		Item:       item,
+		Actor:      actor,
+		IsClaimant: inbox.TagValue(item, "assignee") == actor,
+		BasePath:   s.cfg.basePath,
 	}
-	if provider, ok := s.cfg.contentProviders[item.PayloadType()]; ok {
-		rc := RenderContext{Item: item, Actor: actor, BasePath: s.cfg.basePath}
+
+	provider, ok := s.cfg.contentProviders[item.PayloadType()]
+	if !ok {
+		provider = s.cfg.contentProviders[""]
+	}
+	if provider == nil {
+		return data
+	}
+
+	rc := RenderContext{Item: item, Actor: actor, BasePath: s.cfg.basePath}
+
+	// Check for RichContentProvider first.
+	if rich, ok := provider.(RichContentProvider); ok {
+		result := rich.RenderRich(ctx, rc)
+		data.Content = result.Content
+		data.DrawerSize = result.Size
+		data.Links = result.Links
+	} else {
 		data.Content = provider.Render(ctx, rc)
 	}
-	assignee := inbox.TagValue(item, "assignee")
-	data.IsClaimant = assignee == actor
+
+	// Check for WorkflowStatusProvider.
+	if wsp, ok := provider.(WorkflowStatusProvider); ok {
+		if state, err := wsp.WorkflowStatus(ctx, item); err == nil {
+			data.WorkflowState = &state
+		}
+	}
+
 	return data
 }
 
 type detailData struct {
-	Item       inbox.Item
-	Actor      string
-	IsClaimant bool
-	BasePath   string
-	Content    templ.Component
+	Item          inbox.Item
+	Actor         string
+	IsClaimant    bool
+	BasePath      string
+	Content       templ.Component
+	DrawerSize    DrawerSize
+	Links         []Link
+	WorkflowState *WorkflowState
 }
